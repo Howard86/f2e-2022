fcf52d5ad8178cfbc8a21930d1bb8b6a763606b6
Howard Tai <howard@howardism.dev>
2022-11-05 13:50:25 +0800

    test: add unit test to check html schematics

--- apps/animated-landing/test/index.spec.tsx
+++ apps/animated-landing/test/index.spec.tsx
@@ -1,11 +1,39 @@
 import { render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
 
-describe('docs', () => {
-  it('should render', () => {
+describe('home', () => {
+  it('should have main', () => {
     expect.hasAssertions()
     render(<Home />)
 
-    expect(screen.getByText(/home/i)).toBeInTheDocument()
+    expect(screen.getByRole('main')).toBeInTheDocument()
+  })
+
+  it('should have header', () => {
+    expect.hasAssertions()
+    render(<Home />)
+
+    expect(screen.getByRole('banner')).toBeInTheDocument()
+  })
+
+  it('should have footer', () => {
+    expect.hasAssertions()
+    render(<Home />)
+
+    expect(screen.getByRole('contentinfo')).toBeInTheDocument()
+  })
+
+  it('should have h1', () => {
+    expect.hasAssertions()
+    render(<Home />)
+
+    expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument()
+  })
+
+  it('should have multiple h2', () => {
+    expect.hasAssertions()
+    render(<Home />)
+
+    expect(screen.getAllByRole('heading', { level: 2 })).toHaveLength(6)
   })
 })
