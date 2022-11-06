5e4fcee1f29885f902640d5dfd879925c92f9dc8
Howard Tai <howard@howardism.dev>
2022-11-06 20:46:53 +0800

    chore: update website meta

--- apps/animated-landing/src/pages/_app.tsx
+++ apps/animated-landing/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { Noto_Sans_TC } from '@next/font/google'
 
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 const noto = Noto_Sans_TC({
   variable: '--noto-font',
@@ -17,10 +18,25 @@ const local = localFont({
   weight: '400 700',
 })
 
+const BASE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL || ''
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${local.variable} ${noto.variable} ${noto.className}`}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>F2E 2022</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="4th the F2E 互動式網頁設計，立即報名！" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={BASE_URL} />
+        <meta property="og:title" content="F2E 2022" />
+        <meta property="og:description" content="4th the F2E 互動式網頁設計，立即報名！" />
+        <meta property="og:image" content={`${BASE_URL}/android-chrome-192x192.png`} />
+      </Head>
+      <div className={`relative ${local.variable} ${noto.variable} ${noto.className}`}>
+        <Component {...pageProps} />
+      </div>
+    </>
   )
 }
--- apps/animated-landing/src/pages/index.tsx
+++ apps/animated-landing/src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-
 import Header from '@/components/Header'
 import HomeSection from '@/components/sections/HomeSection'
 import StatementSection from '@/components/sections/StatementSection'
@@ -14,24 +12,18 @@ import Footer from '@/components/Footer'
 export default function HomePage() {
   return (
     <>
-      <Head>
-        <title>F2E 2022</title>
-        <meta name="description" content="4th the F2E 互動式網頁設計，立即報名！" />
-      </Head>
-      <div className="relative">
-        <Header />
-        <main className="flex flex-col gap-6 pt-20">
-          <HomeSection />
-          <StatementSection />
-          <SolutionSection />
-          <TaskSection />
-          <SubmissionSection />
-          <CompetitionSection />
-          <CommonQuestionSection />
-          <SponsorSection />
-        </main>
-        <Footer />
-      </div>
+      <Header />
+      <main className="flex flex-col gap-6 pt-20">
+        <HomeSection />
+        <StatementSection />
+        <SolutionSection />
+        <TaskSection />
+        <SubmissionSection />
+        <CompetitionSection />
+        <CommonQuestionSection />
+        <SponsorSection />
+      </main>
+      <Footer />
     </>
   )
 }
