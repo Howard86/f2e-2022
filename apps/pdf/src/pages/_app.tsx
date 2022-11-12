1aba0d33e40010dad444e7ec262100b05e27ad75
Howard Tai <howard@howardism.dev>
2022-11-12 23:17:40 +0800

    chore: add static assets with html meta

--- apps/pdf/src/pages/_app.tsx
+++ apps/pdf/src/pages/_app.tsx
@@ -1,7 +1,31 @@
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+const BASE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL || ''
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>快點簽 Fast-Sign</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="用簡約、不用過多的裝飾設計，目標是讓使用者可以專注在完成簽署的任務。搭配適當的插畫來表達情境，增加「快點簽」這個產品的情緒與情感的傳達，增進使用者與產品的連結。"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={BASE_URL} />
+        <meta property="og:title" content="快點簽 Fast-Sign" />
+        <meta
+          property="og:description"
+          content="用簡約、不用過多的裝飾設計，目標是讓使用者可以專注在完成簽署的任務。搭配適當的插畫來表達情境，增加「快點簽」這個產品的情緒與情感的傳達，增進使用者與產品的連結。"
+        />
+        <meta property="og:image" content={`${BASE_URL}/android-chrome-192x192.png`} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- apps/pdf/src/pages/_document.tsx
+++ apps/pdf/src/pages/_document.tsx
@@ -4,7 +4,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html className="h-full">
-        <Head />
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0b7d77" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body className="bg-white/90">
           <Main />
           <NextScript />
