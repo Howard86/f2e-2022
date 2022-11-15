959ca290811a6ebd0bcac3c4e71117771c51f20b
Howard Tai <howard@howardism.dev>
2022-11-15 16:57:19 +0800

    test: update HomePage test

--- apps/pdf/test/index.spec.tsx
+++ apps/pdf/test/index.spec.tsx
@@ -2,10 +2,38 @@ import { render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
 
 describe('home', () => {
-  it('should render', () => {
+  it('should have main', () => {
     expect.hasAssertions()
     render(<Home />)
 
-    expect(screen.getByText(/home/i)).toBeInTheDocument()
+    expect(screen.getByRole('main')).toBeInTheDocument()
+  })
+
+  it('should have header', () => {
+    expect.hasAssertions()
+    render(<Home />)
+
+    expect(screen.getByRole('banner')).toBeInTheDocument()
+  })
+
+  it('should have footer', () => {
+    expect.hasAssertions()
+    render(<Home />)
+
+    expect(screen.getByRole('contentinfo')).toBeInTheDocument()
+  })
+
+  it('should have h1', () => {
+    expect.hasAssertions()
+    render(<Home />)
+
+    expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument()
+  })
+
+  it('should have multiple h2', () => {
+    expect.hasAssertions()
+    render(<Home />)
+
+    expect(screen.getAllByRole('heading', { level: 2 })).toHaveLength(5)
   })
 })
