d19f8bfd90fde1d08739576aa61bb2dc5447d50a
Howard Tai <howard@howardism.dev>
2022-11-22 23:11:01 +0800

    feat: set up tailwind css with local font

--- apps/scrum/src/app/layout.tsx
+++ apps/scrum/src/app/layout.tsx
@@ -1,11 +1,28 @@
 import type { ChildrenProps } from 'react'
+import localFont from '@next/font/local'
 
 import '../styles/globals.css'
 
+const local = localFont({
+  src: [
+    {
+      path: './GenJyuuGothic-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './GenJyuuGothic-Bold.woff2',
+      weight: '700',
+      style: 'bold',
+    },
+  ],
+  display: 'fallback',
+})
+
 export default function RootLayout({ children }: ChildrenProps) {
   return (
-    <html lang="zh-Hant-TW" className="h-full">
-      <body className="bg-white/90">{children}</body>
+    <html lang="zh-Hant-TW" className={`bg-primary-main h-full ${local.className}`}>
+      <body className="text-p text-neutral-white-light">{children}</body>
     </html>
   )
 }
--- apps/scrum/tailwind.config.js
+++ apps/scrum/tailwind.config.js
@@ -1,5 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['src/**/*.{ts,tsx}'],
+  theme: {
+    fontSize: {
+      h1: ['3.75rem', { fontWeight: 700 }],
+      h2: ['3rem', { fontWeight: 700 }],
+      h3: ['2rem', { fontWeight: 700 }],
+      h4: ['1.75rem', { fontWeight: 700 }],
+      p: '1.5rem',
+      notice: '1rem',
+    },
+    extend: {
+      colors: {
+        primary: {
+          main: '#70D6DF',
+          dark: '#2C8492',
+        },
+        'secondary-brown': {
+          light: '#8C6A5E',
+          main: '#593C40',
+          dark: '#473438',
+        },
+        'secondary-green': {
+          light: '#80C74F',
+          main: '#42A94E',
+          dark: '#4C793E',
+        },
+        'neutral-white': {
+          light: '#FFFFFF',
+          main: '#DCE9EA',
+          dark: '#A0A9AA',
+        },
+        'neutral-black': {
+          light: '#8D8A91',
+          main: '#726D7A',
+          dark: '#524D5B',
+        },
+      },
+    },
+  },
   plugins: [require('@headlessui/tailwindcss'), require('@tailwindcss/forms')],
 }
