cdcfae3de6565ed8e5705de91cbb6b5253367874
Howard Tai <howard@howardism.dev>
2022-11-22 23:48:50 +0800

    chore: update missing eslint config for tailwind

--- apps/scrum/.eslintrc.js
+++ apps/scrum/.eslintrc.js
@@ -1,5 +1,16 @@
+const path = require('path')
+
 /** @type{import('eslint').ESLint.ConfigData} */
 module.exports = {
   root: true,
   extends: ['f2e-2022'],
+  rules: {
+    'tailwindcss/no-custom-classname': [
+      1,
+      {
+        config: path.join(__dirname, 'tailwind.config.js'),
+        css: path.join(__dirname, 'src/styles/global.css'),
+      },
+    ],
+  },
 }
