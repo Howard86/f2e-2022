571e90847b098fc6dfb7d8d6451e410e119e3fa6
Howard86 <Howard86@users.noreply.github.com>
2022-12-09 16:52:15 +0000

    deploy: 899c2c92604116a1ab10de08e4423922a2ec252e

--- _next/static/aqUelE7mXH2jcGClQjzBV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-edbd22f72bdfb93f.js"],"/_error":["static/chunks/pages/_error-8ba8f2003f3871db.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
