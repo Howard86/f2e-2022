557c242d539255ba45e919249a9b6cfa573604b3
Howard Tai <howard@howardism.dev>
2022-12-09 22:10:29 +0800

    feat: set up github pages deployment with export script

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -2,10 +2,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+const repo = process.env.GITHUB_REPOSITORY?.replace(/.*\//, '')
+
 /** @type{import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: repo ? `/${repo}/` : undefined,
+  basePath: repo ? `/${repo}` : undefined,
+  images: { unoptimized: true },
 }
 
 module.exports = withBundleAnalyzer(config)
